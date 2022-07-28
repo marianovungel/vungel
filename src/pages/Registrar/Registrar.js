a732f60b9b71a0aa9c6bcb183aceb9dd1a4d5907
marianovungel <vungem88@gmail.com>
2022-07-28 15:50:17 -0300

    Alteral o designer do registrar

--- src/pages/Registrar/Registrar.js
+++ src/pages/Registrar/Registrar.js
@@ -3,11 +3,13 @@ import './Registrar.css'
 import {Link, useHistory} from 'react-router-dom'
 import {useState} from 'react'
 import api from '../../services/api.js'
+import Vanilla from 'react-vanilla-tilt'
 
 export default function Registrar() {
 
     const [username, setUsername] = useState("")
     const [whatsapp, setWhatsapp] = useState("")
+    const [email, setEmail] = useState("")
     const [forca, setForca] = useState(0)
     const [password, setPassword] = useState("")
     const [classificacao, setClassificacao] = useState("")
@@ -54,10 +56,9 @@ export default function Registrar() {
         if(classificacao==="Forte" || classificacao==="Excelente"){
             if(confirmPassword===password){
                 try{
-                    const description = Date.now();
                     const res = await api.post("/auth/router/register", {
                         username: username,
-                        email: `${description}@gmail.com`,
+                        email: email,
                         whatsapp: whatsapp,
                         password: password
                     })
@@ -74,23 +75,32 @@ export default function Registrar() {
         }
     }  
   return (
-    <div className="login">
+    <div className="loginRegistrar">
         <div className='content'>
             <div className='logoLoginRes'>
                 <img className='logoImgRes' src='./image/branca.png' alt='logo'/>
             </div>
 
-            <div className='sectionRES'>
+            <div className='sectionREgistrar'>
                 
-                <div className='form'>
-                    <form className='loginForm' onSubmit={handleSubmit}>
-                        <h2>Criar conta</h2>
-                        <input className='inputLogin' type='text' placeholder='  User' onChange={e=>setUsername(e.target.value)} />
-                        <input className='inputLogin' type='Number' placeholder='  8599139-2625' onChange={e=>setWhatsapp(e.target.value)} />
-                        <input className='inputLogin' onKeyUp={validarSenhaForca} type='password' placeholder='  Password' minLength='4' onChange={e=>setPassword(e.target.value)} />
-                        <input className='inputLogin' type='password' placeholder='  Confirme a Senha...' onChange={e=>setConfirmPassword(e.target.value)} />
-                        <button className='inputLogin entrarbutton' type='submit'>Registrar</button>
+                <div className='formregistrar'>
+                    
+
+                    <form className='loginFormregister' onSubmit={handleSubmit}>
+                    <Vanilla id="Valilaclass">
+                        <div className='valillaTilt'>
+                            <h2 className='h2CriarConta'>Criar conta</h2>
+                            <input className='inputLogin' type='text' placeholder='  User' onChange={e=>setUsername(e.target.value)} />
+                            <input className='inputLogin' type='email' placeholder='  your email' onChange={e=>setEmail(e.target.value)} />
+                            <input className='inputLogin' type='Number' placeholder='  8599139-2625' onChange={e=>setWhatsapp(e.target.value)} />
+                            <input className='inputLogin' onKeyUp={validarSenhaForca} type='password' placeholder='  Password' minLength='4' onChange={e=>setPassword(e.target.value)} />
+                            <input className='inputLogin' type='password' placeholder='  Confirme a Senha...' onChange={e=>setConfirmPassword(e.target.value)} />
+                            <button className='inputLogin entrarbutton' type='submit'>Registrar</button>
+                            <h5 className='h6loginregistrar'><Link to='/' className='h6loginregistrar'>Login</Link></h5>
+                        </div>
+                        </Vanilla>
                     </form>
+                    
                     {error && <h3 className='errRegister'>Confirme Corretamente a sua Senha!</h3>}
                     {err && <h3 className='errRegister'>Só é possível registrar com senha Forte ou Excelente</h3>}
                     {classificacao==="Fraca" && <h3 className='errRegister'>Senha: {classificacao}</h3>}
@@ -99,7 +109,7 @@ export default function Registrar() {
                     {classificacao==="Excelente" && <h3 className='errRegister4'>Senha: {classificacao}</h3>}
                     <div className='criar'>
                         <h6 className='Termos'><Link to='#'>Termos e Políticas</Link></h6>
-                         <h6><Link to='/'>Login</Link></h6>
+                         
                     </div>
                 </div>
                 
