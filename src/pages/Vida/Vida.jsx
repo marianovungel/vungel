284bf0bc06762d2ae0008c02e942cd807ad5fe86
marianovungel <vungem88@gmail.com>
2022-09-20 13:24:13 -0300

    Usar links certos

--- src/pages/Vida/Vida.jsx
+++ src/pages/Vida/Vida.jsx
@@ -16,6 +16,18 @@ export default function Vida() {
     const Inter =()=>{
         window.location.replace("https://unilab.edu.br/onibus-intercampi/")
     }
+    const Biblioteca =()=>{
+        window.location.replace("https://unilab.edu.br/biblioteca-universitaria-unilab/")
+    }
+    const Curso =()=>{
+        window.location.replace("https://unilab.edu.br/cursos-de-graduacao/")
+    }
+    const RNE =()=>{
+        window.location.replace("https://www.tirarrne.com.br/artigos/como-renovar-rne-permanente-vencido/")
+    }
+    const Cplp =()=>{
+        window.location.replace("https://unilab.edu.br/paises-parceiros/#:~:text=Tem%20mais%20de%2030%20milh%C3%B5es,s%C3%A9timo%20da%20popula%C3%A7%C3%A3o%20do%20mundo.")
+    }
   return (
     <div className='desapego'>
         <div className='OI' >
@@ -78,80 +90,79 @@ export default function Vida() {
                     <div className="serviceItem">
                         <h6 className="serviceTitle"><i class="fa-solid fa-book"></i> Biblioteca</h6>
                         <p className="serviceParagrafo">
-                            Lorem ipsum dolor 
-                            sit amet consectetur adipisicing
-                            elit. Voluptate ullam qui asperiores veniam
-                            dolores, tempora officia ducimus. Reprehenderit
-                            ad laudantium voluptates necessitatibus 
-                            incidunt iure dolorum vel 
-                            ab! Ad, voluptatibus ex?
+                        O Sistema de Bibliotecas da Unilab (Sibiuni) 
+                        é um órgão suplementar da Unilab criado conforme
+                        artigo 101 do estatuto da Unilab. O Sibiuni é responsável
+                         pelo funcionamento sistêmico das bibliotecas da Unilab, 
+                        a fim de oferecer suporte ao desenvolvimento da pesquisa, 
+                        do ensino e da extensão. O Sibiuni é composto por uma estrutura 
+                        administrativa e 3 bibliotecas Setoriais.
                         </p>
-                        <i className="serviceLink">unilabtem.cf</i>
+                        <i className="serviceLink" onClick={Biblioteca}>Saiba mais...</i>
                     </div>
                     <div className="serviceItem">
                         <h6 className="serviceTitle"><i class="fa-solid fa-bus"></i> Intercampis </h6>
                         <p className="serviceParagrafo">
-                            Lorem ipsum dolor 
-                            sit amet consectetur adipisicing
-                            elit. Voluptate ullam qui asperiores veniam
-                            dolores, tempora officia ducimus. Reprehenderit
-                            ad laudantium voluptates necessitatibus 
-                            incidunt iure dolorum vel 
-                            ab! Ad, voluptatibus ex?
+                        A Coordenação de Serviços Operacionais (CSO), por meio da 
+                        Divisão de Transportes, informa à comunidade 
+                        acadêmica os horários das rotas dos ônibus intercampi.
                         </p>
                         <i className="serviceLink" onClick={Inter}>Saiba mais...</i>
                     </div>
                     <div className="serviceItem">
                         <h6 className="serviceTitle"><i class="fa-solid fa-utensils"></i> Restaurante Universitário (RU)</h6>
                         <p className="serviceParagrafo">
-                            Lorem ipsum dolor 
-                            sit amet consectetur adipisicing
-                            elit. Voluptate ullam qui asperiores veniam
-                            dolores, tempora officia ducimus. Reprehenderit
-                            ad laudantium voluptates necessitatibus 
-                            incidunt iure dolorum vel 
-                            ab! Ad, voluptatibus ex?
+                        Os Restaurantes Universitários (RUs) da 
+                        Unilab têm como objetivos fornecer refeições
+                        nutricionalmente equilibradas, seguras do ponto
+                        de vista higiênico-sanitário e adequadas às necessidades 
+                        nutricionais dos usuários a baixo custo, atuando como um 
+                        dos instrumentos da política de permanência dos jovens na 
+                        educação superior pública federal. São usuários dos RUs 
+                        estudantes dos cursos de graduação ou pós-graduação, na 
+                        modalidade presencial ou à distância, servidores(as) 
+                        técnico-administrativos(as) e docentes, funcionários(as) das 
+                        empresas terceirizadas à serviço da universidade e 
+                        visitantes em caráter acadêmico-cultural.
                         </p>
                         <i className="serviceLink" onClick={Ru}>Saiba mais...</i>
                     </div>
                     <div className="serviceItem">
                         <h6 className="serviceTitle"><i class="fa-solid fa-passport"></i> Documentação Para O Visto</h6>
                         <p className="serviceParagrafo">
-                            Lorem ipsum dolor 
-                            sit amet consectetur adipisicing
-                            elit. Voluptate ullam qui asperiores veniam
-                            dolores, tempora officia ducimus. Reprehenderit
-                            ad laudantium voluptates necessitatibus 
-                            incidunt iure dolorum vel 
-                            ab! Ad, voluptatibus ex?
+                            O processo de Renovação do RNE-CRNM permanente é o mesmo da primeira via do documento. No entanto, esses documentos serão destinados para cada caso. Sendo assim, os documentos básicos, são:
+                            É necessário ter em mãos duas fotos recentes 3×4, coloridas com o fundo branco;
+                            RNE anterior;
+                            Pagamento da taxa requerida para esse documento (levar o comprovante impresso);
+                            Em caso de permanência obtida no Brasil deve levar uma cópia do Diário Oficial da União (DOU);
                         </p>
-                        <i className="serviceLink">unilabtem.cf</i>
+                        <i className="serviceLink" onClick={RNE}>Saiba mais...</i>
                     </div>
                     <div className="serviceItem">
                         <h6 className="serviceTitle"><i class="fa-solid fa-graduation-cap"></i> Cursos de Graduação Unilab</h6>
                         <p className="serviceParagrafo">
-                            Lorem ipsum dolor 
-                            sit amet consectetur adipisicing
-                            elit. Voluptate ullam qui asperiores veniam
-                            dolores, tempora officia ducimus. Reprehenderit
-                            ad laudantium voluptates necessitatibus 
-                            incidunt iure dolorum vel 
-                            ab! Ad, voluptatibus ex?
+                            Na unilab, tem diversos cursos no formato Presencial
+                            e de diversas áreas do saber, que são administrados nos 
+                            campis do Males (Bhaia), nos campis das Auroras e Palmares (Ceará).
                         </p>
-                        <i className="serviceLink">unilabtem.cf</i>
+                        <i className="serviceLink" onClick={Curso}>Saiba mais...</i>
                     </div>
                     <div className="serviceItem">
-                        <h6 className="serviceTitle"><i class="fa-solid fa-flag"></i> PALOPS</h6>
+                        <h6 className="serviceTitle"><i class="fa-solid fa-flag"></i> CPLP</h6>
                         <p className="serviceParagrafo">
-                            Lorem ipsum dolor 
-                            sit amet consectetur adipisicing
-                            elit. Voluptate ullam qui asperiores veniam
-                            dolores, tempora officia ducimus. Reprehenderit
-                            ad laudantium voluptates necessitatibus 
-                            incidunt iure dolorum vel 
-                            ab! Ad, voluptatibus ex?
+                        Um dos propósitos da Unilab é formar 
+                        pessoas aptas para contribuir com a 
+                        integração do Brasil com os países membros
+                        da Comunidade dos Países de Língua Portuguesa
+                        (CPLP), para o desenvolvimento regional, 
+                        científico e educacional, assim como para o
+                        intercâmbio cultural. Esta integração se realizará 
+                        pela composição de corpo docente e discente proveniente
+                        não só das várias regiões do Brasil, mas também de outros
+                        países e do estabelecimento e execução de convênios 
+                        temporários ou permanentes com outras instituições da CPLP.
                         </p>
-                        <i className="serviceLink">unilabtem.cf</i>
+                        <i className="serviceLink" onClick={Cplp}>Saiba mais...</i>
                     </div>
                 </section>
             </div>
