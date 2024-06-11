f33ae2a0e3ceb9aff3b80d48999ff30dc6783331
marianovungel <vungem88@gmail.com>
2024-06-11 01:58:02 -0300

    services

--- src/pages/Home/Home.jsx
+++ src/pages/Home/Home.jsx
@@ -37,6 +37,58 @@ export default function Home() {
                 </div>
             </div>
         </div>
+
+        <div className="service">
+            <div className="marginCenterServece">
+                <div className="textService">
+                    <h3 className="headerAbout">About Me</h3>
+                    <i className="row"></i>
+                    <p className="ParagrafAbout">
+                        Lorem ipsum dolor sit amet consectetur adipisicing elit. Reprehenderit, voluptas vel! Tempore esse minima maxime tenetur sunt, praesentium consequatur.
+                    </p>
+                </div>
+            </div>
+        </div>
+
+        <div className="secundServece">
+            <div className="centerSecundService">
+
+                <div className="CardService">
+                    <div className="backColor">
+                        <i class="fa-solid fa-mobile-screen iconServerSetting"></i>
+                        <h4 className="titleService">App Desig</h4>
+                        <p className="paragrafoServiceCard">Services such as creating modern mobile and web</p>
+                        <p className="paragrafoServiceCard">interface designs.</p>
+                    </div>
+                </div>
+                <div className="CardService">
+                    <div className="backColor">
+                        <i class="fa-solid fa-laptop-code iconServerSetting"></i>
+                        <h4 className="titleService">Web Developement</h4>
+                        <p className="paragrafoServiceCard">Development of web applications aimed at </p>
+                        <p className="paragrafoServiceCard">different areas.</p>
+                    </div>
+                </div>
+                <div className="CardService">
+                    <div className="backColor">
+                        <i class="fa-solid fa-code iconServerSetting"></i>
+                        <h4 className="titleService">Mobile Developement</h4>
+                        <p className="paragrafoServiceCard">Mobile application development Using good</p>
+                        <p className="paragrafoServiceCard">coding practices.</p>
+                    </div>
+                </div>
+                <div className="CardService">
+                    <div className="backColor">
+                        <i class="fa-solid fa-server iconServerSetting"></i>
+                        <h4 className="titleService">Server Developement</h4>
+                        <p className="paragrafoServiceCard">Built the simple and complex server with</p>
+                        <p className="paragrafoServiceCard">Node.js & Express.js</p>
+                    </div>
+                </div>
+
+            </div>
+        </div>
+        
     </div>
   )
 }
