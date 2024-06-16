761f39d60dfcb5b22f59bfe977b259554c5c96f8
marianovungel <vungem88@gmail.com>
2024-06-15 21:12:04 -0300

    navigation

--- src/pages/Home/Home.jsx
+++ src/pages/Home/Home.jsx
@@ -1,4 +1,5 @@
 import React, { useRef, useState } from 'react'
+import { Link } from 'react-router-dom';
 import './Home.css'
 
 const newVar = [
@@ -88,6 +89,15 @@ export default function Home() {
     const [data, setData] = useState(newVar)
     const carrocelVenda = useRef(null)
 
+    function myFunction() {
+        var x = document.getElementById("myLinks");
+        if (x.style.display === "block") {
+          x.style.display = "none";
+        } else {
+          x.style.display = "block";
+        }
+      }
+
     const LeftVenda = (e)=>{
         e.preventDefault()
         carrocelVenda.current.scrollLeft -= carrocelVenda.current.offsetWidth
@@ -96,8 +106,62 @@ export default function Home() {
         e.preventDefault()
         carrocelVenda.current.scrollLeft += carrocelVenda.current.offsetWidth
     }
+
+    const Facebook  = (data)=>{
+        console.log(data)
+        window.open(`${data}`)
+    }
+    const OpenParte  = (data)=>{
+        console.log(data)
+        window.location.replace(`${data}`)
+    }
+
+
+
   return (
     <div className="Home">
+
+        <div className='Humburguer'>
+            <div className="topnav">
+                <a href="#home" className="active">
+                    <img src='./cgadt.png' alt='' className='ImgHumburguer' />
+                </a>
+                <div  className="icon" onClick={myFunction}>
+                    <i className="fa-solid fa-bars iall"></i>
+                </div>
+                <div id="myLinks">
+                    <div onClick={()=>OpenParte("/#About")}>About</div> 
+                    <div onClick={()=>OpenParte("/#Services")}>Services</div>
+                    <div onClick={()=>OpenParte("/#Projects")}>Projects</div>
+                    <div onClick={()=>OpenParte("/#Technologies")}>Technologies</div>
+                    <div onClick={()=>OpenParte("/#CONTACTS")}>Contacts</div>
+                </div>
+            </div>
+        </div>
+
+
+        <div className='Header' id="here">
+            <div className="allcontent">
+                <div className="flag">
+                    <i className="fa-brands fa-square-facebook iconsoc" onClick={()=> Facebook("facebook")}></i>
+                    <i className="fa-brands fa-twitter iconsoc flagItem" onClick={()=> Facebook("facebook")}></i>
+                    <i className="fa-brands fa-square-instagram iconsoc flagItem" onClick={()=> Facebook("facebook")}></i>
+                    <i className="fa-brands fa-youtube iconsoc flagItem" onClick={()=> Facebook("facebook")}></i>
+                </div>
+                <div className="menu">
+                    <div className="itensMenu">
+                        <div onClick={()=>OpenParte("/#About")} className="itemMenu">About</div>
+                        <div onClick={()=>OpenParte("/#Services")} to="/#Services" className="itemMenu">Services</div>
+                        <div onClick={()=>OpenParte("/#Projects")} className="itemMenu">Projects</div>
+                        <div onClick={()=>OpenParte("/#Technologies")} className="itemMenu">Technologies</div>
+                        <div onClick={()=>OpenParte("/#CONTACTS")} className="itemMenu">Contacts</div>
+                    </div>
+                </div>
+
+            </div>
+        </div>
+
+
         <div className="homeIm">
             <div className="first">
                 <small className="Holo">Hello</small>
@@ -112,12 +176,12 @@ export default function Home() {
             </div>
         </div>
 
-        <div className="centerAbout">
+        <div className="centerAbout" id="About">
             <div className="imgSection">
                 <img className="imgAbout" src="./im.png" alt="Mariano António Vunge" />
             </div>
             <div className="textAbout">
-                <h3 className="headerAbout">About Me</h3>
+                <h3 className="headerAbout" id="About">About Me</h3>
                 <i className="row"></i>
                 <p className="ParagrafAbout">
                     Frontend Web and Mobile Developer for 5 years with Javascript fremeworks, database, backend, UI & UX Designer. Ability to solve problems and learn quickly.
@@ -132,7 +196,7 @@ export default function Home() {
             </div>
         </div>
 
-        <div className="service">
+        <div className="service" id='Services'>
             <div className="marginCenterServece">
                 <div className="textService">
                     <h3 className="headerAbout">Services</h3>
@@ -183,7 +247,7 @@ export default function Home() {
             </div>
         </div>
 
-        <div className="projetos">
+        <div className="projetos" id='Projects'>
             <div className="marginCenterServece">
                 <div className="textService">
                     <h3 className="headerAbout">Projects</h3>
@@ -237,7 +301,7 @@ export default function Home() {
             </div>
         </div> 
 
-        <div className="Tecnologias">
+        <div className="Tecnologias" id='Technologies'>
             <h3 className="textTecnologias">Domain Technologies</h3>
         </div>
 
@@ -264,7 +328,63 @@ export default function Home() {
                 <img src="./canva.jpg" alt="" className="capaciti" />
                 <img src="./figma.jpg" alt="" className="capaciti" />
             </div>
-        </div>   
+        </div>
+
+        <div className="contactosContentPage" id="CONTACTS">
+        <div className="fullRed">
+            <div className="telContent">
+                <div className="TitleRed">CONTACTS:</div>
+                <div className="ItemTelRed">
+                    <div className="descItemTelRed">
+                        <i className="fa-solid fa-location-dot sizeRedIcon"></i>
+                        <div className="descTitleRedTell">
+                            <div className="TitleTelItemRed">Address</div>
+                            <div className="EnderecoItemTellRed">Brasil</div>
+                        </div>
+                    </div>
+                    <div className="descItemTelRed">
+                        <i className="fa-solid fa-mobile-screen-button sizeRedIcon"></i>
+                        <div className="descTitleRedTell">
+                            <div className="TitleTelItemRed">Telephones</div>
+                            <div className="EnderecoItemTellRed">( 85 ) 99139 - 2625</div>
+                        </div>
+                    </div>
+                    <div className="descItemTelRed">
+                        <i className="fa-regular fa-envelope sizeRedIcon"></i>
+                        <div className="descTitleRedTell">
+                            <div className="TitleTelItemRed">Email</div>
+                            <div className="EnderecoItemTellRed">vungemariano@gmail.com</div>
+                        </div>
+                    </div>
+                </div>
+            </div>
+            <div className="RedContent">
+                <div className="TitleFacebookRed">WE ARE ON THE NETWORK:</div>
+                <div className="midiasSociasRed">
+                    <i className="fa-brands fa-square-facebook iconsocRed" onClick={()=>Facebook("https://www.linkedin.com/in/mariano-vunge-10283a1b4/")}></i>
+                    <div className="tracoRed"></div>
+                    <i className="fa-brands fa-twitter iconsocRed" onClick={()=>Facebook("x")}></i>
+                    <div className="tracoRed"></div>
+                    <i className="fa-brands fa-square-instagram iconsocRed" onClick={()=>Facebook("instagram")}></i>
+                    <div className="tracoRed"></div>
+                    <i className="fa-brands fa-youtube iconsocRed" onClick={()=>Facebook("youtube")}></i>
+                </div>
+                <div className="divTextRed">
+                    For more information, take a look at my profile on social media...
+                </div>
+            </div>
+        </div>
+      </div>  
+      
+      <div className='Footer'>
+      <div className="contenFooter">
+        <div className="empityFooter"></div>
+        <div className="textFooter">Copyright © 2024 Vungel | Created by 
+        <i className='DevName'>- Mariano A. Vunge</i>
+        </div>
+        <div className="buttonFooter" onClick={()=>OpenParte("/#here")}><i className="fa-solid fa-circle-chevron-up VoltarArrow"></i></div>
+      </div>
+    </div>
     </div>
   )
 }
